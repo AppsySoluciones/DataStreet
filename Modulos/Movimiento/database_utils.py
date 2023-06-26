78cc8e2bd6436a2bd6a9189e4818fb2db6dce31f
Esteban AA <62820991+eathekilla@users.noreply.github.com>
2023-06-26 09:00:36 -0500

    Cargue centros de costo

--- Modulos/Movimiento/database_utils.py
+++ Modulos/Movimiento/database_utils.py
@@ -1,6 +1,7 @@
 from Modulos.UnidadProductiva.models import UnidadProductiva
 from Modulos.UnidadNegocio.models import UnidadNegocio
 from Modulos.CentroCostos.models import CentroCosto,SubCentroCosto
+from django.db.models import Q
 
 import pandas as pd
 
@@ -15,38 +16,71 @@ CentroCosto_lista = CentroCosto.objects.all()
 SubCentroCosto_lista = SubCentroCosto.objects.all()
 
 # Eliminar los duplicados de la columna "columna_a"
-df_sin_duplicados = df['Unidad de Negocio'].drop_duplicates().to_list()
+#df_sin_duplicados = df['Unidad de Negocio'].drop_duplicates().to_list()
 
-print(df_sin_duplicados)
+#print(df_sin_duplicados)
 
-for i in range(len(df)):
-    unegocio = df.iloc[i]['Unidad de Negocio']
-    ccosto = df.iloc[i]['Centro de Costo']
-    subccosto = df.iloc[i]['Subcentro de Costo']
-    
-    print(unegocio,ccosto,subccosto)
+columnas_uprod = df['Unidad de Negocio'].unique()
+columnas_uprod = columnas_uprod.tolist()
 
-    if UnidadesNegocio.filter(nombre=unegocio).exists():
-        unegocio = UnidadesNegocio.filter(nombre=unegocio).first()
-    else:
-        unegocio = UnidadNegocio.objects.create(nombre=unegocio,descripcion=unegocio)
-        unegocio.save()
-    
-    if not CentroCosto_lista.filter(nombre=ccosto).exists():
-        centro = CentroCosto.objects.create(nombre=ccosto)
-        subcentro = SubCentroCosto.objects.create(nombre=subccosto)
-        subcentro.save()
-        centro.unegocio.add(unegocio)
-        centro.subcentro.add(subcentro)
-        centro.save()
-    else:
-        centro = CentroCosto_lista.filter(nombre=ccosto).get()
-        if centro.subcentro.filter(nombre=subccosto).exists():
-            continue
-        else:
-            subcentro = SubCentroCosto.objects.create(nombre=subccosto)
-            centro.subcentro.add(subcentro)
-            centro.unegocio.add(unegocio)
-            centro.save()
+
+for unidad_negocio in columnas_uprod:
+    df_unidad_negocio = df[df['Unidad de Negocio'] == unidad_negocio]
+    for i in range(len(df_unidad_negocio)):
+        unegocio = df_unidad_negocio.iloc[i]['Unidad de Negocio']
+        ccosto = df_unidad_negocio.iloc[i]['Centro de Costo']
+        subccosto = df_unidad_negocio.iloc[i]['Subcentro de Costo']
         
-        centro.save()
+        print(unegocio,ccosto,subccosto)
+
+        if UnidadesNegocio.filter(nombre=unegocio).exists():
+            unegocio = UnidadesNegocio.filter(nombre=unegocio).first()
+        else:
+            unegocio = UnidadNegocio.objects.create(nombre=unegocio,descripcion=unegocio)
+            unegocio.save()
+
+        filtro_ccosto = Q(nombre=ccosto) & Q(unegocio=unegocio)
+        if not CentroCosto_lista.filter(filtro_ccosto).exists():
+            centro = CentroCosto.objects.create(nombre=ccosto)
+            subcentro = SubCentroCosto.objects.create(nombre=subccosto)
+            subcentro.save()
+            centro.unegocio.add(unegocio)
+            centro.subcentro.add(subcentro)
+            centro.save()
+        else:
+            centro = CentroCosto_lista.filter(filtro_ccosto).get()
+            if centro.subcentro.filter(nombre=subccosto).exists():
+                continue
+            else:
+                subcentro = SubCentroCosto.objects.create(nombre=subccosto)
+                centro.subcentro.add(subcentro)
+                centro.unegocio.add(unegocio)
+                centro.save()
+            
+            centro.save()
+
+'''diccionario = {}
+for _, row in df.iterrows():
+    unidad_negocio = row['Unidad de Negocio']
+    centro_costo = row['Centro de Costo']
+    subcentro_costo = row['Subcentro de Costo']
+    
+    if unidad_negocio not in diccionario:
+        diccionario[unidad_negocio] = []
+    
+    centro_costo_encontrado = False
+    for cc in diccionario[unidad_negocio]:
+        if cc['Nombre'] == centro_costo:
+            cc['Subcentros'].append(subcentro_costo)
+            centro_costo_encontrado = True
+            break
+    
+    if not centro_costo_encontrado:
+        diccionario[unidad_negocio].append({
+            'Nombre': centro_costo,
+            'Subcentros': [subcentro_costo]
+        })
+print(diccionario['Administrativa'])
+
+columnas_uprod = df['Unidad de Negocio'].unique()
+columnas_uprod = columnas_uprod.tolist()'''
