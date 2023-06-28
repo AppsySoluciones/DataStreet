0df59a05a706c45668a3ab3be233a1b7b4e5411d
Esteban AA <62820991+eathekilla@users.noreply.github.com>
2023-06-28 14:45:13 -0500

    Update models.py

--- Modulos/Movimiento/models.py
+++ Modulos/Movimiento/models.py
@@ -303,7 +303,7 @@ def get_movimientos_usuario(usuario):
         for unidad_negocio in unidades_negocio:
             condicion = Q(unidad_productiva__in=unidad_negocio.unidades_productivas.all()) 
             union_query |= condicion
-        #union_query |= Q(usuario_presupuesto=usuario)
+        union_query |= Q(usuario_presupuesto=usuario)
         union_query |= Q(usuario_admin_ingreso=usuario)
         return  Movimiento.objects.filter(union_query).all()
     elif usuario.groups.filter(name__in=['Auditor']).exists():
