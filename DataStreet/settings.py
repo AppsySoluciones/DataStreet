21f1e98351dad7007f626b5c8e0a7aa1a55e013a
Esteban AA <62820991+eathekilla@users.noreply.github.com>
2023-09-09 10:10:06 -0500

    fix(servidorlocal)

--- DataStreet/settings.py
+++ DataStreet/settings.py
@@ -186,9 +186,9 @@ AWS_DEFAULT_ACL =  'public-read'
 AWS_S3_VERITY = True
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
-#URL_SERVER = 'http://50.19.129.198:8080/'
+URL_SERVER = 'http://50.19.129.198:8080/'
 
-URL_SERVER = 'http://127.0.0.1:8000/'
+#URL_SERVER = 'http://127.0.0.1:8000/'
 
 
 # Configuración de correo electrónico
